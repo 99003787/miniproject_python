abef71b6b78e01331ae005cb802007a797ae9775
99003743 <78868680+99003743@users.noreply.github.com>
2021-03-17 22:28:52 +0530

    Update code.py

--- 2_design/code.py
+++ 2_design/code.py
@@ -15,3 +15,20 @@ f3 = f1[["REGISTRATION NO",
   
 # creating a new file 
 f3.to_excel("Results.xlsx", index = False) 
+
+//second
+import pandas as pd
+import numpy as np
+all_data = pd.DataFrame()
+for f in glob.glob("../in/sales*.xlsx"):
+    df = pd.read_excel(f)
+    all_data = all_data.append(df,ignore_index=True)
+    all_data.describe()
+    status = pd.read_excel("../in/customer-status.xlsx")
+    all_data_st = pd.merge(all_data, status, how='left')
+all_data_st.head()
+all_data_st[all_data_st["account number"]==737550].head()
+all_data_st['status'].fillna('bronze',inplace=True)
+all_data_st.head()
+all_data_st["status"].cat.set_categories([ "gold","silver","bronze"],inplace=True)
+
